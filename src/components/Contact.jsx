876cc4c11bfe04aa2bfc96dc51e03a6a5265af50
Debashree Jena <jenadeba07@gmail.com>
2024-10-22 15:35:34 +0530

    create form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Contact() {
+  const userData = {
+    userName: "Debashree",
+    password: "Deba@123",
+  };
+
+  const [formData, setFormData] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(formData);
+    setFormData(userData);
+  };
+
   return (
     <>
-      <marque>
+      <marquee>
         <h2>Contact page</h2>
-      </marque>
+      </marquee>
+      <form style={{ display: "flex" }} onSubmit={handleSubmit}>
+        <label>User name:</label>
+        <input
+          type="text"
+          value={formData.userName}
+          onChange={(e) =>
+            setFormData({ ...formData, userName: e.target.value })
+          }
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
