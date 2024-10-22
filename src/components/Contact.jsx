e63cf6bc1a5eca7d1bd5aeca08b3ae4633524fb3
Debashree Jena <jenadeba07@gmail.com>
2024-10-22 15:53:37 +0530

    sign up page complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,47 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 function Contact() {
-  const userData = {
-    userName: "Debashree",
-    password: "Deba@123",
-  };
-
-  const [formData, setFormData] = useState({
-    userName: "",
-    password: "",
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    //console.log(formData);
-    setFormData(userData);
-  };
-
   return (
-    <>
-      <marquee>
-        <h2>Contact page</h2>
-      </marquee>
-      <form style={{ display: "flex" }} onSubmit={handleSubmit}>
-        <label>User name:</label>
-        <input
-          type="text"
-          value={formData.userName}
-          onChange={(e) =>
-            setFormData({ ...formData, userName: e.target.value })
-          }
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          value={formData.password}
-          onChange={(e) =>
-            setFormData({ ...formData, password: e.target.value })
-          }
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </>
+    <marquee>
+      <h2>This is contact page</h2>
+    </marquee>
   );
 }
 
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function SignUp() {
+  const navigate = useNavigate();
+  const userData = {
+    userName: "Debashree",
+    password: "Deba@123",
+  };
+
+  const [formData, setFormData] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(formData);
+    //this is used because if the input data matches with the userData then it navigate to Home page
+    if (
+      userData.userName == formData.userName &&
+      userData.password == formData.password
+    ) {
+      navigate("/");
+    }
+  };
+
   return (
     <>
-      <marque>
-        <h2>This is sign up page</h2>
-      </marque>
+      <marquee>
+        <h2>SignUp page</h2>
+      </marquee>
+      <form onSubmit={handleSubmit}>
+        <label>User name:</label>
+        <input
+          type="text"
+          value={formData.userName}
+          onChange={(e) =>
+            setFormData({ ...formData, userName: e.target.value })
+          }
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
